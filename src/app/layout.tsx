1d955bcf0733b958f2c0a5c9ce1998ac878080f1
MossPipat <pipat@magicsolution.com>
2024-03-04 23:55:53 +0700

    [feat]:setup .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
