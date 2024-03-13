cd38bae2e4b4159bd2b5e816410ea0fbd993329b
MossPipat <pipat@magicsolution.com>
2024-03-13 13:51:18 +0700

    [update]:modify cardlist to tanstack cardlist

--- src/components/Test/BlogListLazyLoadDemo/BlogListLazyLoadDemo.tsx
+++ src/components/Test/BlogListLazyLoadDemo/BlogListLazyLoadDemo.tsx
@@ -1,73 +1,67 @@
 import { Box, Flex, Stack, Text, VStack } from "@chakra-ui/react";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-
-export interface BlogListDemo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
+import { BlogListDemo } from "./type";
+import { Post } from "./Post";
 
 const BlogListLazyLoadDemo = () => {
   const [postList, setPostList] = useState<BlogListDemo[]>();
+  console.log("🚀: ~ postList:", postList);
+
+  const fetchPost = async () => {
+    const res = await axios("https://jsonplaceholder.typicode.com/posts");
+    return res.data;
+  };
+
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPost,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage, pages) => lastPage.nextCursor,
+  });
 
   useEffect(() => {
-    (async () => {
-      const res = await axios("https://jsonplaceholder.typicode.com/posts");
-      const data = res.data;
-      setPostList(data);
-    })();
-  }, []);
+    setPostList(data?.pages[0]);
+  }, [data?.pages]);
+
+  const loadingRender = () => {
+    <p>Loading...</p>;
+  };
+
+  const errorRender = () => {
+    <p>Error: {error?.message}</p>;
+  };
+
+  const postRender = (post: BlogListDemo) => {
+    return <Post post={post} />;
+  };
 
   return (
-    <VStack
-      alignItems="start"
-      overflow="hidden"
-      overflowY="scroll"
-      maxH="100vh"
-      py="24px"
-      gap="16px"
-    >
-      {postList?.map((post) => {
-        return (
-          <Stack
-            key={post.id}
-            border="4px solid blue"
-            borderRadius="40px"
-            p={6}
-            alignItems="start"
-            justifyContent="start"
-            width="100%"
-          >
-            <Flex gap={2}>
-              <Text fontSize={16} fontWeight="bold">
-                user id:
-              </Text>
-              <Text>{post.userId}</Text>
-            </Flex>
-            <Flex gap={2}>
-              <Text fontSize={16} fontWeight="bold">
-                id:
-              </Text>
-              <Text>{post.id}</Text>
-            </Flex>
-            <Flex gap={2}>
-              <Text fontSize={16} fontWeight="bold">
-                title:
-              </Text>
-              <Text>{post.title}</Text>
-            </Flex>
-            <Flex gap={2}>
-              <Text fontSize={16} fontWeight="bold">
-                complete:
-              </Text>
-              <Text>{post.completed}</Text>
-            </Flex>
-          </Stack>
-        );
-      })}
-    </VStack>
+    <>
+      {status === "pending" && loadingRender()}
+      {status === "error" && errorRender()}
+      <VStack
+        alignItems="start"
+        overflow="hidden"
+        overflowY="scroll"
+        maxH="100vh"
+        py="24px"
+        gap="16px"
+      >
+        {postList?.map((post) => {
+          return <Post post={post} />;
+        })}
+      </VStack>
+    </>
   );
 };
 
---
+++ src/components/Test/BlogListLazyLoadDemo/type.ts
@@ -0,0 +1,6 @@
+export interface BlogListDemo {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
