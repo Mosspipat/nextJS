f7bb54a376315db5ac3615c9e7e1652813ba82bd
MossPipat <pipat@magicsolution.com>
2024-03-14 17:11:20 +0700

    [update]:change style card

--- src/components/BlogItem/BlogItem.tsx
+++ src/components/BlogItem/BlogItem.tsx
@@ -34,8 +34,8 @@ export const BlogItem = (props: DetailPost) => {
       pb={6}
       alignItems="start"
       borderRadius={`0px 0px 20px 20px`}
-      border="1px solid rgba(0,0,0,0.2)"
-      boxShadow="10px 10px 10px rgba(0,0,0,0.2)"
+      border="0px solid rgba(0,0,0,0.2)"
+      boxShadow="10px 10px 10px rgba(0,0,0,0.6)"
       transition="all 0.3s ease-out"
       _hover={{
         transform: `scale(1.1)`,
@@ -50,7 +50,7 @@ export const BlogItem = (props: DetailPost) => {
         <Box>
           <Image src={image}  maxW={500} minH={400} objectFit='cover'/>
         </Box>
-        <Stack px={2}>
+        <Stack px={6} >
         <HStack pt={4}>
           <Text 
           style={{ fontWeight: 'bold', borderBottom: '2px solid black'}}
@@ -62,7 +62,7 @@ export const BlogItem = (props: DetailPost) => {
             textOverflow="ellipsis"
             overflow="hidden"
             width={60}
-            color={COLORS.COLOR_CONTRASTS.TERTIARY_COLOR.LightGray}
+            color={COLORS.COLOR_CONTRASTS.QUATERNARY_COLOR.DarkGray}
           >
             {content}
           </Text>
