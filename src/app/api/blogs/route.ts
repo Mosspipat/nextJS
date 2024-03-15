1f01dc15f46a040b537f3ac6656d0b3baca0304d
MossPipat <pipat@magicsolution.com>
2024-03-15 16:21:11 +0700

    [update]:backend API GET:PostList can getparam

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -1,6 +1,17 @@
+import { ParamBlogList } from "@/app/blog/type";
 import { dataPostList } from "@/mock";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET(request: Request) {
+type RequestBlogs = ParamBlogList & NextApiRequest;
+
+export async function GET(request: RequestBlogs) {
+  console.log("getData");
+  const url = new URL(request.url as string);
+  const searchParams = new URLSearchParams(url.searchParams);
+
+  const currentPage = parseInt(searchParams.get("currentPage") || "1");
+  const itemPerPage = parseInt(searchParams.get("itemPerPage") || "10");
+  console.log({ currentPage, itemPerPage });
+
   return Response.json(dataPostList);
 }
--- src/service/getBlogList.tsx
+++ src/service/getBlogList.tsx
@@ -1,10 +1,11 @@
 import { DetailPost } from "@/mock";
 import axios from "axios";
+import { ParamBlogList } from "./type";
 const localUrl = process.env.NEXT_PUBLIC_LOCAL_URL;
 
-export const getBlogs = async (): Promise<DetailPost[]> => {
+export const getBlogs = async ({ currentPage , itemPerPage  }:ParamBlogList): Promise<DetailPost[]> => {
   try {
-    const res = await axios.get(`${localUrl}/api/blogs`);
+    const res = await axios.get(`${localUrl}/api/blogs?currentPage=${currentPage}&itemPerPage=${itemPerPage}`);
     return res.data;
   } catch (error) {
     console.error("error fetch blogs:", error);
