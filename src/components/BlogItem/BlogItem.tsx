cbcf8ba27841850e87b619d8de9faebbab3f8cc7
MossPipat <pipat@magicsolution.com>
2024-03-15 14:06:58 +0700

    [resolve]:remove unnecessary code

--- src/components/BlogItem/BlogItem.tsx
+++ src/components/BlogItem/BlogItem.tsx
@@ -1,12 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Box, HStack, Image as ChakraImage, Stack, Text, VStack } from "@chakra-ui/react";
-import React, { useEffect, useState } from "react";
+import { Box, HStack , Stack, Text, VStack } from "@chakra-ui/react";
+import React from "react";
 import { COLORS } from "@/constant";
 import { DetailPost } from "@/mock";
 import { useRouter } from "next/navigation";
-import { css } from "@emotion/react";
-import Resizer from "react-image-file-resizer";
-import { getImageFileType } from "@/util";
 import { ImageOptimize } from "../ImageOptimize";
 import { PredLoading } from "../PredLoading/PredLoading";
 
@@ -14,70 +11,12 @@ import { PredLoading } from "../PredLoading/PredLoading";
 const BlogItem = (props: DetailPost) => {
   const { id, title, content ,imagePath  } = props;
 
-  const [optimizedImage, setOptimizedImage] = useState<Blob | string | ProgressEvent<FileReader>>();
-
   const router = useRouter();
 
   const handleOnClick = (id: number) => {
     router.push(`/blog/detail/${id}`);
     // router.refresh();
   };
-
-  const [resizedImage, setResizedImage] = useState<string | Blob | ProgressEvent<FileReader>>();
-
-
-  const [width, setWidth] = useState<number>(0);
-  const [height, setHeight] = useState<number>(0);
-
-  useEffect(() => {
-    const img = new Image();
-    img.src = imagePath;
-
-    img.onload = () => {
-      setWidth(img.naturalWidth);
-      setHeight(img.naturalHeight);
-    };
-
-    // Cleanup function to remove event listener and avoid memory leaks
-    return () => {
-      img.onload = null;
-    };
-  }, [imagePath]); 
-
-  //make to util optimize image
-  useEffect(() => {
-    (async () => {
-      const imageFile = await getImageFileType(imagePath);
-      const resizeImage = (imageFile: Blob) => {
-        Resizer.imageFileResizer(
-          imageFile,
-          width/10, // Width
-          height/10, // Height
-          'WEBP', // Format
-          100, // Quality
-          0, // Rotation
-          (resizedImage) => {
-            setResizedImage(resizedImage);
-          },
-          'base64', // Type
-          200, // Min width
-          200 // Min height
-        );
-      };
-      resizeImage(imageFile as Blob); // Call the resizeImage function here
-    })();
-  }, [width,height]);
-
-  const globalStyles = css`
-    @keyframes mymove {
-      from {
-        top: 0px;
-      }
-      to {
-        top: 200px;
-      }
-    }
-  `;
 
   return (
     <VStack
